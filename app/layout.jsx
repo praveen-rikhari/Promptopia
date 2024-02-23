d0af22956f5eb1973727cccf68cd9e5587fa59cd
praveen-rikhari <praveenrikhari45@gmail.com>
2024-02-23 18:28:50 +0530

    Created home page and other components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,24 @@
+import Nav from '@components/Nav';
 import '@styles/globals.css';
 
 export const metadata = {
-    title: "Promptopia",
-    description: "Discover & Share AI Prompts",
-  };
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
 
-  const RootLayout = ({ children }) => (
-    <html lang='en'>
-      <body>
-          <div className='main'>
-            <div className='gradient' />
-          </div>
-  
-          <main className='app'>
-                {children}
-          </main>
-      </body>
-    </html>
-  );
-  
-  export default RootLayout;
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <div className='main'>
+        <div className='gradient' />
+      </div>
+
+      <main className='app'>
+        <Nav/>
+        {children}
+      </main>
+    </body>
+  </html>
+);
+
+export default RootLayout;
